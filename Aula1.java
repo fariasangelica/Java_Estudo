a650c90cceda99d9f3229bffb8c9d25232c67245
Angelica Farias Oliveira <98922466+fariasangelica@users.noreply.github.com>
2022-08-06 22:28:41 -0300

    Add files via upload

---
+++ Aula1.java
@@ -0,0 +1,11 @@
+// Imprimindo uma linha de texto
+
+public class Aula1 {
+	
+//método main inicia a execução do aplicativo Java
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java Programming!");
+	}// fim do método main
+
+}// fim da classe Aula1
