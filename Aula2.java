436534b7780dafffb459c0b62eb88689a74ab17b
Angelica Farias Oliveira <98922466+fariasangelica@users.noreply.github.com>
2022-08-09 15:13:44 -0300

    Add files via upload

---
+++ Aula2.java
@@ -0,0 +1,33 @@
+// Modificando nosso primeiro código
+
+/*public class Aula2 {
+	
+	//método main inicia a execução do aplicativo java
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to ");
+		System.out.println("Java Programming!");
+			
+	} // fim do método main
+
+} //fim da classe Aula2*/
+
+	
+	
+	// Imprimindo múltiplas linhas de texto com uma única instrução
+
+    public class Aula2{
+    	
+    	//método main inicia e execução do aplicativo java
+    	public static void main(String[] args) {
+			
+    		System.out.println("Welcome\nto\nJava\nProgramming!");
+		}// fim do método main
+    	
+    }// fim da classe welcome1
+
+
+
+
+	
+	
