7df6fde481f1ab10a3ba0227f21925bdbc91a583
Angelica Farias Oliveira <98922466+fariasangelica@users.noreply.github.com>
2022-08-09 17:57:49 -0300

    Add files via upload

---
+++ Aula3.java
@@ -0,0 +1,14 @@
+// Exibindo múltiplas linhas com o método System.out.printf
+
+public class Aula3 {
+	
+	//método main inicia a execução do aplicativo java
+	public static void main(String[] args) {
+		
+		System.out.printf("%s%n%s%n",
+				"Welcome to", "Java Programming!");
+		
+			
+	}// dim do método main
+
+}//fim da classe aula3
